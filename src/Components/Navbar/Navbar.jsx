cf10eeb3e1e0bed05ab50ba922c5994e39dcccdc
BhawanaJadaun <bhawanajadaun0001@gmail.com>
2025-05-10 01:07:37 +0530

    push changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -17,13 +17,18 @@ const Navbar = () => {
   return (
    <nav className={`container ${sticky? 'dark-nav' : ''}`}>
 <img src= {logo} alt='' className='logo'/>
-<ul className= {mobileMenu?'':'hide-mobile-menu'}>
-<li><Link to = 'Hero' smooth = {true} offset = {0} duration = {500}>Home</Link></li>
-<li><Link to = 'Program' smooth = {true} offset = {-260} duration = {500}>Program</Link></li>
-<li><Link to = 'About' smooth = {true} offset = {-150} duration = {500}>About us</Link></li>
-<li><Link to = 'Campus' smooth = {true} offset = {-260} duration = {500}>Campus</Link></li>
-<li><Link to = 'Testimonials' smooth = {true} offset = {-260} duration = {500}>Testimonials</Link></li>
-<li><Link to = 'Contact' smooth = {true} offset = {-260} duration = {500}><button className='btn'>Contact us</button></Link></li>
+<ul className={`nav-links ${mobileMenu && window.innerWidth <= 840 ? 'show' : ''}`}>
+<li><Link to='Hero' smooth={true} offset={0} duration={500} onClick={toggleMenu}>Home</Link></li>
+<li><Link to='Program' smooth={true} offset={-260} duration={500} onClick={toggleMenu}>Program</Link></li>
+<li><Link to='About' smooth={true} offset={-150} duration={500} onClick={toggleMenu}>About us</Link></li>
+<li><Link to='Campus' smooth={true} offset={-260} duration={500} onClick={toggleMenu}>Campus</Link></li>
+<li><Link to='Testimonials' smooth={true} offset={-260} duration={500} onClick={toggleMenu}>Testimonials</Link></li>
+<li>
+  <Link to='Contact' smooth={true} offset={-260} duration={500} onClick={toggleMenu}>
+    <button className='btn'>Contact us</button>
+  </Link>
+</li>
+
 </ul>
 <img src = {menu_icon} className='menu_icon' onClick={toggleMenu}/>
    </nav>
--- src/Testimonials/Testimonials.jsx
+++ src/Testimonials/Testimonials.jsx
@@ -1,89 +1,87 @@
-import React, { useRef } from 'react'
-import next_icon from '.././assets/next-icon.png'
-import back_icon from '.././assets/back-icon.png'
-import user1 from '.././assets/user-1.png'
-import user2 from '.././assets/user-2.png'
-import user3 from '.././assets/user-3.png'
-import user4  from '.././assets/user-4.png'
-import './Testimonials.css'
+import React, { useRef, useState, useEffect } from "react";
+import next_icon from "../assets/next-icon.png";
+import back_icon from "../assets/back-icon.png";
+import user1 from "../assets/user-1.png";
+import user2 from "../assets/user-2.png";
+import user3 from "../assets/user-3.png";
+import user4 from "../assets/user-4.png";
+import "./Testimonials.css";
 
 const Testimonials = () => {
-const slider = useRef();
-let tx = 0;
-  const slideForward = ()=> {
-  if(tx > -50){
-   tx -= 25;
-   }      
-   slider.current.style.transform = `translateX(${tx}%)`;
-   }
-  const slideBackward = ()=> {
-     
-   let tx = 0;
-   if(tx < 0){
-   tx += 25;
-   }      
-   slider.current.style.transform = `translateX(${tx}%)`;
-  }
+  const slider = useRef();
+  const [tx, setTx] = useState(0);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 800);
 
-   
+  // Update isMobile on window resize
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 800);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const slideForward = () => {
+    if (isMobile) return;
+    if (tx > -50) {
+      const newTx = tx - 25;
+      setTx(newTx);
+      slider.current.style.transform = `translateX(${newTx}%)`;
+    }
+  };
+
+  const slideBackward = () => {
+    if (isMobile) return;
+    if (tx < 0) {
+      const newTx = tx + 25;
+      setTx(newTx);
+      slider.current.style.transform = `translateX(${newTx}%)`;
+    }
+  };
+
   return (
-    <div className='Testimonials'>
-<img src= {next_icon} alt='' className='next-btn' onClick={slideForward}/>
-<img src= {back_icon} alt='' className='back-btn' onClick={slideBackward}/>
-<div className='slider'>
-<ul ref={slider}>
-<li>
-<div className='slide'>
-<div className='user-info'>
-<img src= {user1} alt=''/>
-<div>
-<h3>William Jackson1</h3>
-<span>Edusity,USA</span>
-</div>
-</div>
-<p>Choosing to persue my degree at Edusity was one of the best decision I've ever made. This supportive community, state-of-the-art facilities , and commitment to academic excellence have truly exceeded</p>
-</div>
-</li>
-<li>
-<div className='slide'>
-<div className='user-info'>
-<img src= {user2} alt=''/>
-<div>
-<h3>William Jackson2</h3>
-<span>Edusity,USA</span>
-</div>
-</div>
-<p>Choosing to persue my degree at Edusity was one of the best decision I've ever made. This supportive community, state-of-the-art facilities , and commitment to academic excellence have truly exceeded</p>
-</div>
-</li>
-<li>
-<div className='slide'>
-<div className='user-info'>
-<img src= {user3} alt=''/>
-<div>
-<h3>William Jackson3</h3>
-<span>Edusity,USA</span>
-</div>
-</div>
-<p>Choosing to persue my degree at Edusity was one of the best decision I've ever made. This supportive community, state-of-the-art facilities , and commitment to academic excellence have truly exceeded</p>
-</div>
-</li>
-<li>
-<div className='slide'>
-<div className='user-info'>
-<img src= {user4} alt=''/>
-<div>
-<h3>William Jackson4</h3>
-<span>Edusity,USA</span>
-</div>
-</div>
-<p>Choosing to persue my degree at Edusity was one of the best decision I've ever made. This supportive community, state-of-the-art facilities , and commitment to academic excellence have truly exceeded</p>
-</div>
-</li>
-</ul>
-</div>
+    <div className="Testimonials">
+      {!isMobile && (
+        <>
+          <img
+            src={next_icon}
+            alt=""
+            className="next-btn"
+            onClick={slideForward}
+          />
+          <img
+            src={back_icon}
+            alt=""
+            className="back-btn"
+            onClick={slideBackward}
+          />
+        </>
+      )}
+      <div className="slider">
+        <ul ref={slider}>
+          {[user1, user2, user3, user4].map((userImg, index) => (
+            <li key={index}>
+              <div className="slide">
+                <div className="user-info">
+                  <img src={userImg} alt="" />
+                  <div>
+                    <h3>William Jackson{index + 1}</h3>
+                    <span>Edusity, USA</span>
+                  </div>
+                </div>
+                <p>
+                  Choosing to pursue my degree at Edusity was one of the best
+                  decisions I've ever made. This supportive community,
+                  state-of-the-art facilities, and commitment to academic
+                  excellence have truly exceeded my expectations.
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
